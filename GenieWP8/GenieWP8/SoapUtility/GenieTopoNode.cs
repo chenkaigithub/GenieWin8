16af54dd6682a0b3a8cbf107e33e74d8624e1978
yankai <yk_kai@163.com>
2013-08-02 18:06:58 +0800

    add get router image path

---
+++ GenieWP8/GenieWP8/SoapUtility/GenieTopoNode.cs
@@ -0,0 +1,36 @@
+﻿
+using System.IO;
+using System.Text.RegularExpressions;
+namespace GenieWP8
+{
+    class ImageNameGenerator
+    {
+        private string imagePath;
+        private string routerName;
+        public ImageNameGenerator(string model)
+        { 
+            Regex regx = new Regex("-|_");
+            int index = regx.Match(model).Index;
+            if (index > 0)
+            {
+                routerName = model.Substring(0, index);
+            }
+            regx = new Regex("v(\\d+)",RegexOptions.IgnoreCase);
+            Match match = regx.Match(routerName);
+            routerName = routerName.Replace(match.ToString(), "").ToUpper();
+            if (routerName == "" || routerName == "N/A")
+            {
+                routerName = "DEFAULT_NETGEAR";
+            }
+        }
+        public string getImagePath()
+        {
+            imagePath = string.Format("Assets/router/{1}",routerName);
+            if (!File.Exists(imagePath))
+            {
+                imagePath = string.Format("Assets/router/{1}", "DEFAULT_NETGEAR");
+            }
+            return imagePath;
+        }
+    }
+}
